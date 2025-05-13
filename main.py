3fdf7d17ff3048ffed24722fbe95f6237be9daec
Gabriel Terra <gabrielsantosterra@gmail.com>
2025-05-13 15:54:10 -0300

    Habilitado cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database.connection import engine
 from app.models import user  # importa o módulo completo, não uma classe
 from app.routers import user as usuario_router  # este é seu routers/user.py
@@ -7,6 +8,14 @@ from app.routers import user as usuario_router  # este é seu routers/user.py
 user.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # ou ["*"] para liberar geral
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(usuario_router.router)
 
 @app.get("/")
