8063a5095c0df34e52e3555ddf235d2bc8f52da5
Gabriel Terra <gabrielsantosterra@gmail.com>
2025-06-17 16:35:35 -0300

    Habilitado cors para o frontend

--- main.py
+++ main.py
@@ -3,20 +3,27 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.database.connection import engine
 from app.models import user  # importa o módulo completo, não uma classe
 from app.routers import user as usuario_router  # este é seu routers/user.py
-from app.routers import ged as ged_router  # este é seu routers/ged.py
+from app.routers import ged as ged_router       # este é seu routers/ged.py
 from app.routers import document as documents_router
 
 # cria as tabelas a partir das classes dentro de user.py
 user.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# ✅ Configuração CORS para frontend local e produção Firebase Hosting
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # ou ["*"] para liberar geral
+    allow_origins=[
+        "http://localhost:5173",            # desenvolvimento local
+        "https://docrh-615cf.web.app"       # produção (Firebase Hosting)
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# rotas
 app.include_router(documents_router.router)
 app.include_router(usuario_router.router)
 app.include_router(ged_router.router)
